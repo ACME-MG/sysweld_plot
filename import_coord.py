ba4fecc60f2c53002723e8c1e4bcd41a54868885
Wendy-Ji <j.wendy234@gmail.com>
2023-03-16 10:49:57 +1100

    Added import based on visual weld or assembly

--- import_coord.py
+++ import_coord.py
@@ -1,17 +1,24 @@
 #%% Import libraries
 import pandas as pd
 #%%
-def import_coord(file):
+def import_coord(file,sysweld_app):
     """
     This script imports the coordinates of all the nodes.
     
     Required arguments:
         file: imported file
+        sysweld_app: application used to generate results file
     Returns:
         coord: coordinates of nodes
     """
     #Coordinates of nodes
-    coord=file['SYSWELD']['constant']['entityresults']['NODE']['COORDINATE']['ZONE1_set0']['erfblock']['res']
+    if sysweld_app=='weld':#Choose path to coordinates in results file 
+        coord=file['SYSWELD']['constant']['entityresults']['NODE']['COORDINATE']['ZONE1_set0']['erfblock']['res']
+    elif sysweld_app=='assembly':
+        coord=file['CSMIMPL']['constant']['entityresults']['NODE']['COORDINATE']['ZONE1_set0']['erfblock']['res']
+    else:
+        print('Please choose weld or assembly')
+
     coord=pd.DataFrame(coord) #Convert to dataframe
     coord=coord.rename(columns={0: 'X', 1: 'Y', 2: 'Z'}) #rename columns X,Y,Z
 
