688871d7a321e7dabe150489c445eca85abc41e6
Wendy Ji <61012460+Wendy-Ji@users.noreply.github.com>
2023-08-22 10:37:33 +1000

    Update import_file.py
    Update arguments setup

--- import_file.py
+++ import_file.py
@@ -1,17 +1,16 @@
-
 #%% Import libraries
 import os
 import h5py
-#%% Function
-def import_file(path,file_name):
+#%% 
+def import_file(path, file_name):
     """
     This script imports the required results file.
 
     Required arguments:
-        path: path to results file
-        file_name: name of results file
+        path (string):      path to results file, e.g. 'C:/Users/...'
+        file_name (string): name of results file
     Returns:
-        file: imported results file
+        file (HDF5 group):  imported results file
     """
     home_path=os.getcwd() #get current path
     os.chdir(path) #change to path to results file
