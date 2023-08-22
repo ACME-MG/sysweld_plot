9fc5a55c47058e1d0f3de7a1cd45e4baf990d8a5
Wendy Ji <61012460+Wendy-Ji@users.noreply.github.com>
2023-08-22 10:36:29 +1000

    Update import_coord.py
    Update sysweld_app

--- import_coord.py
+++ import_coord.py
@@ -1,25 +1,28 @@
 #%% Import libraries
 import pandas as pd
 #%%
-def import_coord(file,sysweld_app):
+def import_coord(file, sysweld_app):
     """
-    This script imports the coordinates of all the nodes.
+    This script puts the coordinates of all the nodes into a dataframe.
     
     Required arguments:
-        file: imported file
-        sysweld_app: application used to generate results file
+        file (HDF5 group):      imported results file
+        sysweld_app (string):   application used to generate results file ('min_weld' for minimum results file from visual weld, 
+                                                                        'weld' for normal results file from visual weld, or 
+                                                                        'assembly' for results file from visual assembly)
     Returns:
-        coord: coordinates of nodes
+        coord (dataframe):      coordinates of nodes (columns: X,Y,Z)
     """
-    #Coordinates of nodes
-    if sysweld_app=='weld':#Choose path to coordinates in results file 
-        coord=file['SYSWELD']['constant']['entityresults']['NODE']['COORDINATE']['ZONE1_set0']['erfblock']['res']
-    elif sysweld_app=='assembly':
-        coord=file['CSMIMPL']['constant']['entityresults']['NODE']['COORDINATE']['ZONE1_set0']['erfblock']['res']
+    # Path to coordinates
+    if sysweld_app == 'min_weld' or sysweld_app == 'weld':
+        coord = file['SYSWELD']['constant']['entityresults']['NODE']['COORDINATE']['ZONE1_set0']['erfblock']['res']
+    elif sysweld_app == 'assembly':
+        coord = file['CSMIMPL']['constant']['entityresults']['NODE']['COORDINATE']['ZONE1_set0']['erfblock']['res']
     else:
-        print('Please choose weld or assembly')
+        print('Please choose "min_weld", "weld", or "assembly"')
 
-    coord=pd.DataFrame(coord) #Convert to dataframe
-    coord=coord.rename(columns={0: 'X', 1: 'Y', 2: 'Z'}) #rename columns X,Y,Z
+    # Convert to dataframe
+    coord = pd.DataFrame(coord) #Convert to dataframe
+    coord = coord.rename(columns={0: 'X', 1: 'Y', 2: 'Z'}) #rename columns X,Y,Z
 
     return coord
