64201043cecdf6277e58a2905cd5374348831967
Wendy Ji <61012460+Wendy-Ji@users.noreply.github.com>
2023-08-22 10:42:14 +1000

    Update plot_variable.py
    Updated argument set up

--- plot_variable.py
+++ plot_variable.py
@@ -5,70 +5,74 @@ from scipy.interpolate import griddata
 import matplotlib.colors as colors
 #%% Plot variable on cross section figure
 #Function to plot variable on cross section figure
-def plot_variable(coord_sec,Xgrid,Ygrid,fig,ax,mask,section_axes=None,
-    parameter_sec=None,variable=None,plot_title=None,figure_no=None,
-    title_text=18,
-    max_data=None,min_data=None,cbar = True,
-    cMin=0,cMax=100,cticks_no=11,clevels_no=100,cbar_over_value=None,cscheme='jet',cbar_extend='both',cnorm=None,
-    cbar_text=18,cbar_length=5,cbar_tick_width=1, cbar_title=None,
-    cbar_bottom_offset=0.4,cbar_height=0.12,
-    folder=None,export_name=None): 
+def plot_variable(coord_sec, Xgrid, Ygrid, fig, ax, mask, section_axes = None,
+    parameter_sec = None, variable = None, min_data = None, max_data = None,
+    figure_no = 0, title_vis = True, plot_title = 'Title of Plot', title_textsize = 18,
+    cnorm = False, cMin = 0, cMax = 100, cticks_no = 11, cbar_over_value = None,clevelMin = None, clevelMax = None, clevels_no = 100,cscheme = 'jet',
+    cbar = True, cbar_extend = 'both', cbar_bottom_offset = 0.1, cbar_height = 0.05, cbar_extend_frac = 0.03,
+    cbar_text = 18, cbar_length = 5, cbar_tick_width = 1, cbar_title = 'Colour Bar Title', cbar_title_pad = 10,
+    folder = None, export_name = 'Colour Map of Cross Section'): 
     """
-    Required Arguments:
-        coord_sec: coordinates of cross section
-        Xgrid,Ygrid: meshgrid to extrapolate data to
-        fig,ax: figure and axes cross section are plotted on
-        mask: coordinates of points inside outline (same dimension as Xgrid and Ygrid)
-        section_axes: axes of cross section, e.g. ['X','Y','Z']
-    Optional Arguments:
-        Plot:
-            paramater: dataframe name, default: None
-            variable: column name in dataframe, e.g. 'XX'
-            plot_title: title for the plot, e.g. 'Transverse Stress'
-            figure_no: figure number to be plotted on, e.g. 0
-        Title/Axes:
-        title_text: fontsize of title, default: 18
+    Arguments:
+
+        coord_sec (dataframe) :         coordinates of nodes cross section
+        Xgrid,Ygrid:                    meshgrid to extrapolate data to
+        fig,ax:                         figure and axes cross section are plotted on
+        mask:                           coordinates of points inside outline (same dimension as Xgrid and Ygrid)
+        section_axes (list):            list of three axes with plot on first two axes and normal to third axis, default: ['X','Y','Z']
+        
         Data:
-            max_data: maximum value data can have (remove errors from extrapolating above max_data)
-            min_data: minimum value data can have (remove errors from extrapolating below min_data)
+            parameter_sec (dataframe):  parameter values at each node
+            variable (string):          column name in dataframe ('HV', 'XX', 'YY', 'ZZ', 'Phase 1', 'Phase 2', etc., 'Temp', 'strain')
+            min_data (float):           minimum value data can have (remove errors from extrapolating below min_data), default: None
+            max_data (float):           maximum value data can have (remove errors from extrapolating above max_data), default: None
+        Plot:
+            figure_no (int):            figure number for cross section plot, default 0
+            title_vis (bool):           whether to plot title, default: True
+            plot_title (string):        title for the plot, default: 'Title of Plot'
+            title_textsize:             fontsize of title, default: 18
+        Colour Map:
+            cnorm (bool):               setting midpoint of colour bar to zero, default: False
+            cMin (float):               minimum value for the colour bar ticks, default: 0
+            cMax (float):               maximum value for the colour bar ticks, default: 100
+            cticks_no (float):          number of ticks on the colour bar, default: 11
+            cbar_over_value (float):    maximum value before colour bar changes to grey, default: None
+            clevelMin (float):          minimum value for the colour map levels, default: cMin
+            clevelMax (float):          maximum value for the colour map levels, default: cMax
+            clevels_no (float):         number of levels for the colour map, default: 100
+            cscheme (string):           colour scheme of colour bar, default: jet
         Colour Bar:
-            cbar: whether to plot colour bar, default: True
-            cMin: minimum value for the colour bar, default: 0
-            cMax: maximum value for the colour bar, default: 100
-            cticks_no: number of ticks on the colour bar, default: 11
-            clevels_no: number of levels on the colour bar, default: 100
-            cbar_over_value: value to change colour bar to grey, default: None
-            cscheme: colour scheme of colour bar, default: jet
-            cnorm: normalisation of colour bar, default: None
-            cbar_extend: colourbar ends, default: 'both', takes arguments of: 'neither', 'both', 'min', 'max'
-            cbar_text: fontsize of colour bar ticks, default: 18
-            cbar_length: length of colour bar ticks, default: 5
-            cbar_width: width of colour bar ticks, default: 1
-            cbar_title: title of colour bar, default: None
-        Colour Bar Position:
-            cbar_bottom_offset: offset of the colour bar from the bottom of the plot (% of height of plot), default: 0.4
-            cbar_height: height of the colour bar (% of height of plot), default: 0.12
+            cbar (bool):                whether to plot colour bar, default: True
+            cbar_extend (string):       colourbar ends, default: 'both', takes arguments of: 'neither', 'both', 'min', 'max'
+            cbar_bottom_offset (float:  offset of the colour bar from the bottom of the plot (% of height of plot), default: 0.1
+            cbar_height (float):        height of the colour bar (% of height of plot), default: 0.05
+            cbar_extend_frac (float):   fraction of colour bar to extend for arrows, default: 0.03
+        Colour Bar Text/Ticks
+            cbar_text (float):          fontsize of colour bar ticks, default: 18
+            cbar_length (float):        length of colour bar ticks, default: 5
+            cbar_width (float):         width of colour bar ticks, default: 1
+            cbar_title (string):        title of colour bar, default: 'Colour Bar Title'
+            cbar_title_pad (float):     padding between colour bar title and colour bar, default: 10
         Save:
-            folder: folder to save plot to, default: None
-            export_name: name of exported file, default: None
+            folder (string):            path to folder to save plot to, default: None
+            export_name (string):       name of exported file, default: 'Colour Map of Cross Section'
     """
-
-    nan_ind=parameter_sec[variable].isnull() #indices where there are nan values
-
-    Zgrid=griddata((coord_sec.loc[~nan_ind,section_axes[0]],coord_sec.loc[~nan_ind,section_axes[1]]),parameter_sec.loc[~nan_ind,variable], (Xgrid, Ygrid), method='cubic') #plot non-nan values
-
-    if min_data!=None or max_data!=None: #if max_data or min_data has a value, clip Zgrid to those values
+    # Extrapolate parameter values on cross section
+    nan_ind = parameter_sec[variable].isnull() # indices of nodes where there are nan values
+    Zgrid=griddata((coord_sec.loc[~nan_ind,section_axes[0]],coord_sec.loc[~nan_ind,section_axes[1]]),\
+                   parameter_sec.loc[~nan_ind,variable], (Xgrid, Ygrid), method='cubic') # extrapolate non-nan parameter values to meshgrid
+    if min_data != None or max_data != None: #if max_data or min_data has a value, remove any errors in extrapolation outside that range
         Zgrid=np.clip(Zgrid,min_data,max_data)
-
+    Zgrid = np.ma.masked_where(~mask, Zgrid) #mask data outside of outline
     print('Extrapolated '+variable+' values to meshgrid')
 
-    Zgrid = np.ma.masked_where(~mask, Zgrid) #mask data outside of outline
+    # Figure to plot on
+    plt.figure(figure_no) # plot on figure no
+    if title_vis == True: # plot title visiblity
+        plt.title(plot_title, fontsize = title_textsize) 
 
-    #Plot variable
-    plt.figure(figure_no) #Plot on figure 0
-    plt.title(plot_title, fontsize = title_text) #plot title
-
-    class MidpointNormalize(colors.Normalize): #normalise levels for colour bar
+    # Colour bar
+    class MidpointNormalize(colors.Normalize): # class for setting midpoint value of colour bar
         def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
             self.midpoint = midpoint
             colors.Normalize.__init__(self, vmin, vmax, clip)
@@ -76,37 +80,41 @@ def plot_variable(coord_sec,Xgrid,Ygrid,fig,ax,mask,section_axes=None,
         def __call__(self, value, clip=None):
             x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
             return np.ma.masked_array(np.interp(value, x, y))
-
-    if cnorm!=None: #if cnorm has a value, normalise to eor midpoint
-        cnorm=MidpointNormalize(vmin=cMin,vmax=cMax,midpoint=0)
         
-    cticks=np.linspace(cMin,cMax,cticks_no) #Colour bar ticks
-    
-    if cbar_over_value!=None: #if cbar_over_value is not None, add cbar_over_value to colour bar ticks
-        cticks=np.append(cticks,cbar_over_value)
+    if cnorm == True: # normalise to zero midpoint
+        cnorm = MidpointNormalize(vmin=cMin,vmax=cMax,midpoint=0)
+    elif cnorm == False: # no normalisation
+        cnorm = None
+
+    cticks = np.linspace(cMin,cMax,cticks_no) # colour bar ticks
+
+    if cbar_over_value != None: # colour bar is grey over this value
+        cticks=np.append(cticks,cbar_over_value) # add value to colour bar ticks
         cMax=cbar_over_value #set cMax to cbar_over_value
         cscheme=plt.get_cmap('jet').copy() #copy colour scheme
         cscheme.set_over('grey') #set colour bar over cbar_over_value to grey
 
-    levels = np.linspace(cMin, cMax, clevels_no) #Colour bar levels
-    im=plt.contourf(Xgrid, Ygrid, Zgrid, levels, vmin=cMin, vmax=cMax, cmap=cscheme,extend=cbar_extend,norm=cnorm) #Plot variable
+    if clevelMin == None: # set minimum colour bar level to tick minimum value
+        clevelMin = cMin
+    if clevelMax == None: # set maximum colour bar level to tick maximum value
+        clevelMax = cMax
 
+    levels = np.linspace(clevelMin, clevelMax, clevels_no) # colour bar levels do not need to match ticks
+    im = plt.contourf(Xgrid, Ygrid, Zgrid, levels, vmin = cMin, vmax = cMax, cmap = cscheme,extend = cbar_extend,norm = cnorm) # plot colour map
+
+    # Add colour bar in axes below the plot of cross section
     if cbar == True:
-        #colour bar in new axes
-        left, bottom, width, height = ax.get_position().bounds #position of plot
-        cax = fig.add_axes([left, bottom-cbar_bottom_offset, width, height * cbar_height]) #position and size of new plot
-        
-        cbar=plt.colorbar(im, norm=cnorm,orientation='horizontal', cax=cax,ticks=cticks,extend=cbar_extend,extendfrac=0.03) #fraction of colour bar for extend arrows
-
-        cbar.ax.tick_params(labelsize=cbar_text,length=cbar_length,width=cbar_tick_width) #colour bar tick size
-
-        if cbar_title!=None: #colourbar title below colourbar
-            cbar.ax.set_xlabel(cbar_title,fontsize=cbar_text,labelpad=10)
-
-        plt.savefig(folder+export_name+' Map', bbox_inches='tight', dpi=300)
-
-        cbar.remove() #Remove plot and colourbar for next plot
+        left, bottom, width, height = ax.get_position().bounds # position of cross section plot
+        cax = fig.add_axes([left, bottom-cbar_bottom_offset, width, height * cbar_height]) # position and size of new plot for colour bar
+        cbar = plt.colorbar(im, norm = cnorm, orientation = 'horizontal', cax = cax,ticks = cticks,
+                          extend = cbar_extend, extendfrac = cbar_extend_frac) # plot colour bar
+        cbar.ax.tick_params(labelsize = cbar_text, length = cbar_length, width = cbar_tick_width) # format colour bar ticks 
+        if cbar_title != None: # colourbar title visibility
+            cbar.ax.set_xlabel(cbar_title, fontsize = cbar_text, labelpad = cbar_title_pad) # colourbar title
+        plt.savefig(folder + export_name, bbox_inches = 'tight', dpi = 300) # save figure
+        cbar.remove() # remove colourbar for next plot
     else:
         plt.savefig(folder+export_name+' Map', bbox_inches='tight', dpi=300)
+        cbar.remove() # remove colourbar for next plot
     
-    print('Plotted map for '+plot_title)
+    print('Plotted map')
